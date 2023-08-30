ecfa3886cdf7c3246cf2299ef8f7342888b2aa83
Kizilov Andrey <irosmith@yandex.ru>
2023-08-30 08:13:03 +0300

    Доработки 2

--- src/utils/MoviesApi.js
+++ src/utils/MoviesApi.js
@@ -1,30 +1,25 @@
-export class Api {
+class Api {
   constructor({ address }) {
     this._address = address;
   }
 
   //обработка запроса
-  _request(url, options) {
-    const fetchAddress = `${url}`;
-
-    return fetch(fetchAddress, options).then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        return Promise.reject(`Ошибка: ${res.status}`);
-      }
-    });
+  _request(res) {
+    if (!res.ok) {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+    return res.json();
   }
 
   //Метод для запроса массива всех карточек стороннего Api
   getArrayCards() {
-    return this._request(this._address, {
+    return fetch(`${this._address}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(),
-    });
+    }).then(this._request);
   }
 }
 const movieApi = new Api({
